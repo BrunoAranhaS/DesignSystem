0d747d80bc9eb140b2526991990eabc31b897b25
BrunoAranhaS <brunoaranhasouza02@gmail.com>
2022-10-14 19:55:46 -0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/global.css"
+
+export function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h1 className="text-2xl text-cyan-500">Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
